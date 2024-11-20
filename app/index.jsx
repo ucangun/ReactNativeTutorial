985f969c0268f5860cbc2094ff77772ec496ec3b
ucangun <149247682+ucangun@users.noreply.github.com>
2024-11-20 23:24:00 +0100

    "Image , BackgroundImage , uploading Networks Images {uri: ..... }"

--- app/index.jsx
+++ app/index.jsx
@@ -1,13 +1,22 @@
 // import { Link } from "expo-router";
-import { View, Text } from "react-native";
+import { View, Text, Image, ImageBackground } from "react-native";
+const logoImg = require("../assets/adaptive-icon.png");
 
 export default function Home() {
   return (
     <View style={{ flex: 1, backgroundColor: "plum", padding: "60" }}>
-      <Text>
+      {/* <Text>
         <Text style={{ color: "white" }}>Hello </Text>
         World
-      </Text>
+      </Text> */}
+      {/* <Image source={logoImg} style={{ width: 300, height: 300 }} /> */}
+      {/* <Image
+        source={{ uri: "https://picsum.photos/500" }}
+        style={{ width: 300, height: 300, objectFit: "cover" }}
+      /> */}
+      <ImageBackground source={logoImg} style={{ flex: 1 }}>
+        <Text>Image</Text>
+      </ImageBackground>
     </View>
   );
 }
