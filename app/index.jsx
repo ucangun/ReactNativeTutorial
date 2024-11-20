a2a239d70a62113d03843f8a498d85d17f443935
ucangun <149247682+ucangun@users.noreply.github.com>
2024-11-20 22:49:01 +0100

    "Core Component Text"

--- app/index.jsx
+++ app/index.jsx
@@ -3,13 +3,11 @@ import { View, Text } from "react-native";
 
 export default function Home() {
   return (
-    <View style={{ flex: 1, backgroundColor: "plum" }}>
-      <View
-        style={{ width: 200, height: 200, backgroundColor: "lightblue" }}
-      ></View>
-      <View
-        style={{ width: 200, height: 200, backgroundColor: "lightgreen" }}
-      ></View>
+    <View style={{ flex: 1, backgroundColor: "plum", padding: "60" }}>
+      <Text>
+        <Text style={{ color: "white" }}>Hello </Text>
+        World
+      </Text>
     </View>
   );
 }
